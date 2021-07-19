1ae46015238753a049d266adcd3ed2614e1d8ab4
Muhammad <muhammad.ksht@gmail.com>
2021-07-19 13:34:28 +0430

    implemented the first page

---
+++ menbar_application/lib/firstpage/Orators.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class Orators extends StatelessWidget {
+  const Orators({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+          'Orators'
+      ),
+    );
+  }
+}
