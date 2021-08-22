2ceadb20d61213b86ac77aed74411d58df22e0e5
Muhammad <muhammad.ksht@gmail.com>
2021-08-22 21:52:38 +0430

    added the icon changing of button

--- menbar_application/lib/collections/play_button.dart
+++ menbar_application/lib/collections/play_button.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 
-class PlayButton extends StatelessWidget {
+class PlayButton extends StatefulWidget {
   const PlayButton({Key? key}) : super(key: key);
+  @override
+  _PlayButtonState createState() => _PlayButtonState();
+}
 
+class _PlayButtonState extends State<PlayButton> {
+  bool isPlaying = false;
   @override
   Widget build(BuildContext context) {
     return Container(
       child: OutlinedButton(
         onPressed: (){
-
         },
         style: OutlinedButton.styleFrom(
           shape: CircleBorder(),
           backgroundColor: Colors.white38,
         ),
         child: IconButton(
-          icon: Icon(Icons.play_arrow,size:25,color: Colors.black54,),
+          icon: isPlaying ? Icon(Icons.play_arrow,size:25,color: Colors.black54,) :
+          Icon(Icons.pause,size:25,color: Colors.black54,),
+          splashColor: Colors.transparent,
           onPressed: () {
+            setState(() {
+              isPlaying = !isPlaying;
+            });
           },
           alignment: Alignment.center,
         ),
